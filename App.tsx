b7c441830b8f10f32e680ec13648aacaaacd4c00
plusbeauxjours <plusbeauxjours@gmail.com>
2021-06-12 14:14:52 +0700

    set: transitions

--- App.tsx
+++ App.tsx
@@ -1,112 +1,49 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-
 import React from 'react';
-import type {Node} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+import {StatusBar} from 'react-native';
+import {createStackNavigator} from '@react-navigation/stack';
+import {enableScreens} from 'react-native-screens';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+import Transitions from './src/Transitions';
 
-const Section = ({children, title}): Node => {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
+import StyleGuide from './src/components/StyleGuide';
+
+enableScreens();
+
+const fonts = {
+  'SFProText-Bold': require('./assets/fonts/SF-Pro-Text-Bold.otf'),
+  'SFProText-Semibold': require('./assets/fonts/SF-Pro-Text-Semibold.otf'),
+  'SFProText-Regular': require('./assets/fonts/SF-Pro-Text-Regular.otf'),
 };
 
-const App: () => Node = () => {
-  const isDarkMode = useColorScheme() === 'dark';
+const assets = [];
 
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
+const Stack = createStackNavigator();
+const AppNavigator = () => (
+  <React.Fragment>
+    <Stack.Navigator
+      screenOptions={{
+        headerStyle: {
+          backgroundColor: StyleGuide.palette.primary,
+          borderBottomWidth: 0,
+        },
+        headerTintColor: 'white',
+      }}>
+      <Stack.Screen
+        name="Transitions"
+        component={Transitions}
+        options={{
+          title: 'Transitions',
+        }}
+      />
+    </Stack.Navigator>
+  </React.Fragment>
+);
 
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.js</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
-};
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
+const App = () => (
+  <React.Fragment>
+    <StatusBar barStyle="light-content" />
+    <AppNavigator />
+  </React.Fragment>
+);
 
 export default App;
